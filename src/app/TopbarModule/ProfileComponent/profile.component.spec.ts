2395c97df3db317cc6f8dea068761734a02f1e91
Валерий Лазакович < >
2016-09-27 19:45:54 +0300

    Add unit tests for profile component

---
+++ src/app/TopbarModule/ProfileComponent/profile.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
+
+import { ProfileComponent } from './profile.component';
+
+describe('AppComponent: ', () => {
+  let fixture: ComponentFixture<ProfileComponent>;
+  let comp: ProfileComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfileComponent]
+    });
+
+    fixture = TestBed.createComponent(ProfileComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it ('should be created', () => {
+    expect(comp instanceof ProfileComponent).toBe(true, 'should create ProfileComponent');
+  });
+
+  it('should contain element with class "profile"', () => {
+    let debugProfile: DebugElement = fixture.debugElement.query(By.css('.profile'));
+    let elemProfile: HTMLElement = debugProfile.nativeElement;
+
+    expect(elemProfile).toBeDefined();
+  });
+});
