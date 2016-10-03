1fe2670c42f8202cc09b4b5def57600605faf600
Валерий Лазакович < >
2016-10-03 13:23:24 +0300

    Add tasks service for getting tasks from remote server

---
+++ src/app/shared/TasksService/tasks.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {Headers, Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import {Task} from '../TaskObjInterface/task-obj.interface.ts';
+
+@Injectable()
+export class TasksService {
+  private _uri = 'https://api.mlab.com/api/1/databases/dashboard_tasks/collections/tasks';
+  private _apiKey = 'wYOXfeNqx8m2P7fxATUeF4QSU7qoC9RT';
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http){};
+
+  getTasks(): Promise<Task[]> {
+    return this.http.get(`${this._uri}?apiKey=${this._apiKey}`)
+      .toPromise()
+      .then(response => response.json().data as Task[]);
+  };
+
+  addTask(task: Task): Promise<Task> {
+    return this.http.post(`${this._uri}?apiKey=${this._apiKey}`, JSON.stringify(task), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data);
+  };
+
+  updateTask(task: Task): Promise<Task> {
+    let id = task._id.$oid;
+    return this.http.put(`${this._uri}/${id}?apiKey=${this._apiKey}`, JSON.stringify(task), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data);
+  };
+
+  deleteTask(id: string): Promise<Task> {
+    return this.http.delete(`${this._uri}/${id}?apiKey=${this._apiKey}`, {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data);
+  };
+};
